4d97375bfd10bf5c2f3a190e848bed64fecb5f48
jeffe <jeffe@LAPTOP-KHGKRI9S>
2021-04-30 23:41:29 -0500

    Agregar archivos de proyecto.

---
+++ NunezJefferson_Examen/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NunezJefferson_Examen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string contraseña = txtPass.Text;
+
+            if (string.IsNullOrEmpty(txtUser.Text))
+            {
+                await DisplayAlert("Aviso", "Debe ingresar un usuario", "Aceptar");
+                txtUser.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(txtPass.Text))
+            {
+                await DisplayAlert("Aviso", "Debe ingresar una contraseña", "Aceptar");
+                txtPass.Focus();
+                return;
+            }
+            else
+            {
+                if ((usuario == "estudiante2021") && (contraseña == "uisrael2021"))
+                {
+                    await Navigation.PushAsync(new Registro(txtUser.Text));
+                }
+                else
+                {
+                    lblResultado.Text = "El usuario o contraseña son incorrectos";
+                }
+            }
+        }
+    }
+}
---
+++ NunezJefferson_Examen/Registro.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NunezJefferson_Examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblTexto.Text = "Usuario conectado: " + usuario;
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+
+
+            try
+            {
+                double pagoinicial = Convert.ToDouble(txtMontoinicial.Text);
+                double montomensual = Convert.ToDouble(txtPagoMensual.Text);
+
+                double cuota = ((montomensual + 90) * 3) + pagoinicial;
+                txtPagoMensual.Text = cuota.ToString();
+
+
+                await DisplayAlert("Alerta", "Elemento Guardado con Exito", "Aceptar");
+                await Navigation.PushAsync(new Resumen(txtNombre.Text, txtPagoMensual.Text));
+
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Aviso", "Ingrese los datos", "Aceptar");
+            }
+
+        }
+    }
+
+
+}
